9eda8316a27ad17dbda10aab879c7233f4c618fe
Jerry Liu <liujerry7@gmail.com>
2025-01-26 00:04:24 -0800

    Player jump

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -6,8 +6,21 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
+    public bool facingRight = true;
+
     public float moveSpeed = 5f;
     private float xMovement;
+
+    public float jumpPower = 10f;
+
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
+    public float baseGravity = 2f;
+    public float maxFallSpeed = 18f;
+    public float fallSpeedMultiplier = 2f;
 
     void Start()
     {
@@ -17,10 +30,68 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(xMovement * moveSpeed, rb.velocity.y);
+        Gravity();
+        Flip();
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         xMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!isGrounded())
+        {
+            return;
+        }
+
+        if (context.performed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            animator.SetTrigger("jump");
+        }
+    }
+
+    private void Gravity() 
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
+    }
+
+    private void Flip()
+    {
+        if (facingRight && xMovement < 0 || !facingRight && xMovement > 0)
+        {
+            facingRight = !facingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapBox(
+            groundCheckPos.position, 
+            groundCheckSize, 
+            0, 
+            groundLayer
+        );
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
